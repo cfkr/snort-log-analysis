c6e5fd60a35fac96c161dc310500e84895615017
cfkr <65411249+cfkr@users.noreply.github.com>
2026-03-10 00:43:06 +0300

    Update upgraded_logoku.sh

--- upgraded_logoku.sh
+++ upgraded_logoku.sh
@@ -12,17 +12,19 @@ ANAHTAR_KELIMELER=("WARNING" "ALERT" "ERROR")
 while IFS= read -r satir
 do
     for kelime in "${ANAHTAR_KELIMELER[@]}" do
-    # "${ANAHTAR_KELIMELER[@]}" ifadesi dizideki tüm elemanları birer birer temsil eder.Yani döngü her turda dizinin bir elemanını kelime isimli değişkene atar.
-        # Eğer satır anahtar kelimeyi içeriyorsa
         if [[ $satir == *"$kelime"* ]]; then 
-        #*"$kelime"* ifadesi, $satir içinde $kelime kelimesinin herhangi bir yerde geçip geçmediğini kontrol eder.* dan dolayı bunu yazdık - Bu biçim sadece [[ ... ]] içinde bu şekilde çalışır.
             echo "$satir" >> "$ONEMLI_LOG"  
-            # $satir değişkeninde tutulan metni, $ONEMLI_LOG dosyasının sonuna ekliyor.
             break 
-            #Satırda ilk anahtar kelimeyi bulur bulmaz, break ile diğer kelimelere bakmayıp döngüyü hemen durduruyoruz.
         fi
     done
 done < "$LOG_DOSYASI" 
-#while döngüsünün içine giren kod bloğu, standart girdi (standard input) olarak belirtilen dosyadan satır satır veri okur.
 
 echo "Analiz tamamlandı. Önemli kayıtlar $ONEMLI_LOG dosyasına yazıldı."
+
+
+ # "${ANAHTAR_KELIMELER[@]}" ifadesi dizideki tüm elemanları birer birer temsil eder.Yani döngü her turda dizinin bir elemanını kelime isimli değişkene atar.
+        # Eğer satır anahtar kelimeyi içeriyorsa
+        #*"$kelime"* ifadesi, $satir içinde $kelime kelimesinin herhangi bir yerde geçip geçmediğini kontrol eder.* dan dolayı bunu yazdık - Bu biçim sadece [[ ... ]] içinde bu şekilde çalışır.
+            # $satir değişkeninde tutulan metni, $ONEMLI_LOG dosyasının sonuna ekliyor.
+            #Satırda ilk anahtar kelimeyi bulur bulmaz, break ile diğer kelimelere bakmayıp döngüyü hemen durduruyoruz.
+#while döngüsünün içine giren kod bloğu, standart girdi (standard input) olarak belirtilen dosyadan satır satır veri okur.
